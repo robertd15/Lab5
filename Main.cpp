83cde4ac0f7b37536b1759d2bc85f19041f9108b
shooyin <j_le_75@hotmail.com>
2018-03-12 22:04:25 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Node.h"
+#include "BST.h"
 #include "Person.h"
 
 using namespace std;
@@ -7,11 +7,33 @@ using namespace std;
 int main() {
 
 	Person *mark = new Person("Mark", "Zuckerberg", "19840514");
+	Person *bill = new Person("Bill", "Gates", "19551028");
+	Person *steve = new Person("Steve", "Jobs", "19550224");
 
-	Node<Person> *root = NULL;
-	root->insert(root, *mark);
+	/*
+	BST<Person> *root = new BST<Person>();
+	root->add(*mark);
+	root->add(*bill);
+	root->add(*steve);
+	*/
+	//cout << *mark;
 
-	root->traverse(root);
+	//root->print();
+
+	BST<int> t;
+	t.insert(20);
+	t.insert(25);
+	t.insert(15);
+	t.insert(10);
+	t.insert(30);
+	t.display();
+	t.remove(20);
+	t.display();
+	t.remove(25);
+	t.display();
+	t.remove(30);
+	t.display();
+
 	system("pause");
 	return 0;
 }
